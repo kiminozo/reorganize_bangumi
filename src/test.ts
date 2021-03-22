cd663c1455f40247f4b62cf10ad610741c93574d
kiminozo <kiminozoeien@gmail.com>
2021-03-22 11:24:33 +0800

    file demo

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,25 @@
 import { MovieDb } from 'moviedb-promise'
 //import axios, { AxiosProxyConfig } from 'axios'
 import { httpsOverHttp } from 'tunnel'
-
+import * as fs from "fs"
+import * as readline from "readline";
+import colors = require('colors/safe');
 
 const apiKey: string = "5501399346685e41aa3df9c47ed4671f";
 const db = new MovieDb(apiKey);
 
 const proxy = {
     host: "127.0.0.1",
-    port: 10080,
+    port: 7890,
 };
 
 const axiosConfig = { httpsAgent: httpsOverHttp({ proxy: proxy }) };
 const params = {
     language: 'zhCN'
 }
-async function test() {
+async function tvInfo(word: string) {
     try {
-        let res = await db.searchTv({ query: '无职', language: 'zh-CN' }, axiosConfig);
+        let res = await db.searchTv({ query: word, language: 'zh-CN' }, axiosConfig);
         console.log(res);
 
         //if (res.results && res.results.length > 0) {
@@ -29,16 +31,20 @@ async function test() {
         console.log(error);
     }
 }
+const regex = /\[[^\[\]]+\]\S+?\[?([^\[\]]+)\]?.+/;
 
-// async function test2() {
-//     try {
-//         let res = await axios.get("https://api.github.com/feeds",
-//             { httpsAgent: httpsOverHttp({ proxy: proxy }) });
-//         console.log(res.data);
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
+function test() {
+    // await fs.readFile("tests/files.txt");
+    let readInterface = readline.createInterface(fs.createReadStream("tests/files.txt"), process.stdout);
+    readInterface.on('line', function (line) {
+        let result = regex.exec(line);
+        if (result) {
+            console.log(colors.green(result[1]));
+        }
+    });
+}
 
-console.log("hello world");
+console.log(colors.yellow("hello world"));
 test();
+//tvInfo("无职");
+
