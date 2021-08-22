3ce791697d93d38bd4a4405f3fe068fa38646a56
Kiminozo <kiminozo.eien@gmail.com>
2021-08-22 23:48:28 +0800

    epnames

--- src/nfoConveter.ts
+++ src/nfoConveter.ts
@@ -14,7 +14,12 @@ async function testRead(): Promise<Item> {
     return item
 }
 
-async function findNames(path: string, epCount: number): Promise<string[]> {
+interface EpName {
+    name: string
+    ep: number
+}
+
+async function findNames(path: string, epCount: number): Promise<EpName[]> {
     const files = await fs.promises.readdir("tests")
     //console.log(files)
     const nameList: string[] = []
@@ -31,14 +36,69 @@ async function findNames(path: string, epCount: number): Promise<string[]> {
     }
     const sortNameList = nameList.sort((a, b) => b.length - a.length)
     const mainName = sortNameList[epCount / 2]
-    console.log(mainName)
+    // console.log(mainName)
     const match = findBestMatch(mainName, nameList)
-    const result = match.ratings.filter(p => p.rating > 0.9)
-    // .flatMap(p => p.rating)
-    console.log(result)
-    return nameList
+    const names = match.ratings.filter(p => p.rating > 0.9)
+        .flatMap(p => p.target)
+    // console.log(names)
+    const keys = numIndex(mainName);
+    //console.log(keys)
+    const info = numIndex(mainName).find(d => {
+        const tmp = names.map(n => n.substring(0, d.index))
+        //console.log(tmp);
+        return isSame(tmp)
+    })
+
+    //[Airota & Nekomoe kissaten][Machikado Mazoku][01][720p][CHS]
+    //console.log(info)
+    const epNames = names.flatMap(n => { return { ep: epNum(n, info.index), name: n } })
+        .sort((a, b) => a.ep - b.ep)
+    return epNames
 
 }
+
+interface NumIndexData {
+    key: string
+    index: number
+}
+
+
+function numIndex(name: string): NumIndexData[] {
+    const result: NumIndexData[] = []
+    const regexp = /\d+/g;
+    let m: RegExpExecArray
+    do {
+        m = regexp.exec(name);
+        if (m) {
+            m[0]
+            result.push({ key: m[0], index: m.index })
+        }
+    } while (m);
+    return result;
+}
+
+function epNum(name: string, index: number): number {
+    const test = name.substring(index, name.length - 1)
+    //console.log(test)
+    const regexp = /\d+(\.\d)?/g;
+    const m = regexp.exec(test);
+    if (m) {
+        //  console.log(m[0])
+        return parseFloat(m[0])
+    }
+
+}
+
+
+function isSame(names: string[]) {
+    return names.reduce((a, b) => {
+        if (a.val !== b) {
+            a.same = false
+        }
+        return a
+    }, { same: true, val: names[0] }).same
+}
+
 
 function conveter(item: Item) {
     const title = item.name_cn != null ? item.name_cn : item.name
@@ -91,7 +151,7 @@ async function test() {
 
 async function test2() {
     const names = await findNames("tests", 12)
-    // console.log(names)
+    console.log(names)
 }
 
 test2()
