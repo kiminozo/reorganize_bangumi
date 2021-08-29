ebedcc633cef504a34a1a900272c553ffc826ad7
Kiminozo <kiminozo.eien@gmail.com>
2021-08-29 19:20:06 +0800

    bug

--- src/reorganize.ts
+++ src/reorganize.ts
@@ -3,7 +3,7 @@ import { scan, move } from './files'
 import * as fs from "fs"
 import Path = require('path');
 import { infoApi } from "./bgm"
-import { makeNfo } from "./nfoConveter";
+import { makeNfo, matchNames } from "./nfoConveter";
 const dataFileName = "data.json";
 
 const output = "output.log";
@@ -43,13 +43,16 @@ async function reorganizePath(root: string, path: string): Promise<void> {
 
 async function updateInfo(path: string): Promise<void> {
     const item = await readData(path);
+    console.log(path)
     if (!item) {
         return;
     }
-    // const epItems = item.eps as EpItem[]
-    // if (epItems && epItems.length != 0) {
-    //     return;
-    // }
+    const epItems = item.eps as EpItem[]
+    if (epItems[0]) {
+
+        return;
+    }
+    console.log("更新：" + path)
     const newItem = await infoApi(item.id)
     if (!newItem) {
         return;
@@ -67,28 +70,105 @@ async function reorganizeAll(root: string, deep: number) {
 async function updateInfoAll(root: string, deep: number) {
     const dirs = await scan(root, deep);
     console.log(dirs)
+    //await waitAllLimit(dirs.map(dir => updateInfo(dir)), 1)
     for (const dir of dirs) {
-        console.log(dir)
+        //console.log(dir)
         await updateInfo(dir)
         //await makeNfo(dir)
     }
+    // await Promise.all(dirs.map(dir => updateInfo(dir)));
+
 }
 
 async function updateNfoAll(root: string, deep: number) {
     const dirs = await scan(root, deep);
-    // for (const dir of dirs) {
-    //     console.log(dir)
-    //     await makeNfo(dir)
-    // }
-    await Promise.all(dirs.map(dir => makeNfo(dir)));
+    for (const dir of dirs) {
+        console.log(dir)
+        await updateNfo(dir)
+    }
+    //   await waitAllLimit(dirs, 2, path => updateNfo(path));
 }
 
+async function checkNfo(path: string) {
 
+}
+
+async function updateNfo(path: string) {
+    let exist = false
+    try {
+        const files = await fs.promises.readdir(path)
+        const mapFiles = files.filter(file => Path.extname(file) === ".nfo")
+        exist = mapFiles.length > 8
+        if (exist) {
+            console.log(mapFiles)
+            return
+        }
+    } catch (error) {
+    }
+    await makeNfo(path)
+}
+
+async function waitAllLimit<T, R>(source: T[], limit: number, func: (item: T) => Promise<R>): Promise<void> {
+    // const result: T[] = []
+    while (source.length > 0) {
+        // 100 at at time
+        const children = source.splice(0, limit);
+        const childrenResult = await Promise.all(children.map(func));
+        // result.push(...childrenResult)
+    }
+    // return result;
+}
+
+async function flatFile(path: string) {
+    //let epNames = []
+    console.log(path)
+    try {
+        const files = await fs.promises.readdir(path)
+        const names: string[] = []
+        for (const file of files) {
+            if ((await fs.promises.lstat(Path.join(path, file))).isDirectory()) {
+                names.push(file)
+            }
+        }
+        const epNames = matchNames(names)
+        console.log(epNames)
+        for (const ep of epNames) {
+            const children = await fs.promises.readdir(Path.join(path, ep.name))
+            for (const child of children) {
+                await fs.promises.rename(Path.join(path, ep.name, child),
+                    Path.join(path, child))
+            }
+            await fs.promises.rmdir(Path.join(path, ep.name))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function loadError(file: string) {
+    const data = await fs.promises.readFile(file, 'utf-8');
+    const errors = data.split("\r\n").filter(p => p !== "")
+    //console.log(errors)
+    const items = errors.map(line => {
+        const s = line.split(":")
+        return { path: s[0], type: s[1] }
+    }).filter(p => p.type == "BDRip")
+    for (const item of items) {
+        //await makeNfo(item.path)
+        await fs.promises.rename(item.path, Path.join("/Volumes/anime/重新整理", Path.basename(item.path)))
+    }
+}
 
 //reorganizeAll(Path.join("/Volumes/pt", "新番"), 1)
 //updateInfoAll(Path.join("/Volumes/pt", "新番"), 2)
 //console.log("fate/stay night".replace(/\//gi, " "));
-updateNfoAll(Path.join("/Volumes/anime", "2020s"), 1)
-//updateInfoAll(Path.join("/Volumes/anime/", "2020s",), 1)
+
+//updateNfoAll(Path.join("/Volumes/anime", "2010s"), 1)
+
+
+//updateInfoAll(Path.join("/Volumes/anime/", "2010s",), 1)
 //updateInfo(Path.join("/Volumes/anime/2020s/", "2020-04", "ARTE"))
-//makeNfo(Path.join("/Volumes/anime/2020s/2020-04/噬血狂袭 IV"))
+//makeNfo("/Volumes/anime/2010s/2013-04/打工吧！魔王大人")
+
+//flatFile(Path.join("/Volumes/anime/2010s", "2019-10/高分少女 第二季"))
+//loadError("nfo-output.log")
